0a2953fc7d06654fae27e050ac816fd6bf9b784c
Rik Puts <rik.puts@student.kdg.be>
2022-11-14 16:36:04 +0100

    reflections package toegevoegd

--- src/main/java/DIByRik/DIByRikApplication.java
+++ src/main/java/DIByRik/DIByRikApplication.java
@@ -1,4 +1,7 @@
 package DIByRik;
 
+import org.reflections.Reflections;
+
 public class DIByRikApplication {
+    private final Reflections reflections = new Reflections("");   //TODO package prefix configurable (or check if that's nessesary)
 }
